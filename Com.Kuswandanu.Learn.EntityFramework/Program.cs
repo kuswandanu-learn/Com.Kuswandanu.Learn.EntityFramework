f5bb57c9471b348a666fd0c22d11365b40513c85
kuswandanu <kuswandanu@gmail.com>
2018-12-06 09:17:49 +0700

    Add project files.

---
+++ Com.Kuswandanu.Learn.EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Kuswandanu.Learn.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
